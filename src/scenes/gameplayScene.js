0d31a37aebf839c714ce8ad7119f3ed1f6b1d65d
Phildo <phildo211@gmail.com>
2017-05-19 11:53:04 -0500

    advance back and forth

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -14,7 +14,7 @@ var GamePlayScene = function(game, stage)
   var dragger;
   var keyer;
 
-  var shift;
+  var keys;
 
   var hover_xval;
   var hover_yval;
@@ -32,9 +32,14 @@ var GamePlayScene = function(game, stage)
 
   var limitGraph = function()
   {
+    if(my_graph.disp_max_xv > my_graph.xv[my_graph.xv.length-1])
+    {
+      my_graph.disp_min_xv -= my_graph.disp_max_xv-my_graph.xv[my_graph.xv.length-1];
+      my_graph.disp_max_xv = my_graph.xv[my_graph.xv.length-1];
+    }
     if(my_graph.disp_min_xv > my_graph.disp_max_xv-hr)   my_graph.disp_min_xv = my_graph.disp_max_xv-hr;
     if(my_graph.disp_min_xv < my_graph.disp_max_xv-year) my_graph.disp_min_xv = my_graph.disp_max_xv-year;
-    if(my_graph.disp_max_xv > my_graph.xv[my_graph.xv.length-1]) my_graph.disp_max_xv = my_graph.xv[my_graph.xv.length-1];
+    my_graph.dirty = true;
   }
 
   var getDataPt = function(ts)
@@ -53,9 +58,9 @@ var GamePlayScene = function(game, stage)
           {
             my_graph.insertDataNext(ts,r[from][to],0,1);
             my_graph.clampDisp();
-            limitGraph();
             my_graph.disp_min_yv = 0;
             my_graph.disp_max_yv *= 1.1;
+            limitGraph();
           }
         }
       }
@@ -100,9 +105,9 @@ var GamePlayScene = function(game, stage)
             }
             my_graph.insertDataBlockNext(x,y,0,1);
             my_graph.clampDisp();
-            limitGraph();
             my_graph.disp_min_yv = 0;
             my_graph.disp_max_yv *= 1.1;
+            limitGraph();
           }
         }
       }
@@ -117,12 +122,33 @@ var GamePlayScene = function(game, stage)
     dragger = new Dragger({source:stage.dispCanv.canvas});
     keyer = new Keyer({source:stage.dispCanv.canvas});
 
-    shift = new function()
+    keys = new function()
     {
       var self = this;
-      self.down = false;
-      self.key_down = function(evt){ if(evt.keyCode == 16) self.down = true; };
-      self.key_up   = function(evt){ if(evt.keyCode == 16) self.down = false; };
+      self.shift = false;
+      self.key_down = function(evt)
+      {
+        var delta;
+        switch(evt.keyCode)
+        {
+          case 16: //shift
+            self.shift = true;
+          break;
+          case 37: //left
+            delta = my_graph.disp_max_xv-my_graph.disp_min_xv;
+            my_graph.disp_min_xv -= delta;
+            my_graph.disp_max_xv -= delta;
+            limitGraph();
+          break;
+          case 39: //right
+            delta = my_graph.disp_max_xv-my_graph.disp_min_xv;
+            my_graph.disp_min_xv += delta;
+            my_graph.disp_max_xv += delta;
+            limitGraph();
+          break;
+        }
+      };
+      self.key_up   = function(evt){ if(evt.keyCode == 16) self.shift = false; };
     }
 
     my_graph = new variable_graph();
@@ -156,19 +182,18 @@ var GamePlayScene = function(game, stage)
     {
       var xt = (evt.doX-my_graph.x)/my_graph.w;
       var new_drag_xval = mapVal(0,1,my_graph.disp_min_xv,my_graph.disp_max_xv,xt);
-      if(shift.down)
-      {
-        my_graph.disp_min_xv -= new_drag_xval-drag_xval;
-        my_graph.disp_max_xv -= new_drag_xval-drag_xval;
-      }
-      else
+      if(keys.shift)
       {
         var old_xt = mapVal(my_graph.disp_min_xv,my_graph.disp_max_xv,0,1,drag_xval);
         if(xt > 1) return;
         my_graph.disp_min_xv = mapVal(my_graph.disp_max_xv,new_drag_xval,my_graph.disp_max_xv,drag_xval,my_graph.disp_min_xv);
       }
+      else
+      {
+        my_graph.disp_min_xv -= new_drag_xval-drag_xval;
+        my_graph.disp_max_xv -= new_drag_xval-drag_xval;
+      }
       limitGraph();
-      my_graph.dirty = true;
     }
     my_graph.dragFinish = function(evt)
     {
@@ -190,14 +215,12 @@ var GamePlayScene = function(game, stage)
     hoverer.flush();
     dragger.filter(my_graph);
     dragger.flush();
-    keyer.filter(shift);
+    keyer.filter(keys);
     keyer.flush();
   };
 
   self.draw = function()
   {
-    my_graph.draw(ctx);
-
     ctx.textAlign = "right";
     ctx.font = "20px Arial";
     ctx.fillText("$"+fdisp(my_graph.yv[my_graph.yv.length-1]), my_graph.x+my_graph.w, my_graph.y-20);
@@ -213,6 +236,9 @@ var GamePlayScene = function(game, stage)
       var date = new Date(floor(hover_xval)*1000);
       ctx.fillText(dateToString(date),my_graph.x,my_graph.y-15);
       y = mapVal(my_graph.disp_min_yv, my_graph.disp_max_yv, my_graph.y+my_graph.h, my_graph.y, hover_yval);
+      ctx.beginPath();
+      ctx.arc(hover_pos.x,y,5,0,twopi);
+      ctx.stroke();
       ctx.beginPath();
       ctx.moveTo(my_graph.x           ,y);
       ctx.lineTo(my_graph.x+my_graph.w,y);
@@ -260,7 +286,6 @@ var GamePlayScene = function(game, stage)
     ctx.lineWidth = 0.5;
     if(purchases)
     {
-      ctx.beginPath();
       for(var i = 0; i < purchases.length; i++)
       {
         var date = new Date(purchases[i].date);
@@ -270,13 +295,20 @@ var GamePlayScene = function(game, stage)
         y = mapVal(my_graph.disp_min_yv, my_graph.disp_max_yv, my_graph.y+my_graph.h, my_graph.y, val);
         ctx.fillText("$"+fdisp(val), x+2,y-30);
         ctx.fillText(dateToString(date),x+2,y-15);
+        ctx.beginPath();
+        ctx.arc(x,y,5,0,twopi);
+        ctx.stroke();
+        ctx.beginPath();
         ctx.moveTo(my_graph.x           ,y);
         ctx.lineTo(my_graph.x+my_graph.w,y);
         ctx.moveTo(x,my_graph.y);
         ctx.lineTo(x,my_graph.y+my_graph.h);
+        ctx.stroke();
       }
-      ctx.stroke();
     }
+
+    my_graph.draw(ctx);
+
   };
 
   self.cleanup = function()
